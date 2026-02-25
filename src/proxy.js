7f1d5e518a93297da9f206ef913b8ff07da43b0c
minions-03 <kumarmukesh515151@gmail.com>
2026-02-25 12:45:48 +0530

    proxy file updated

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
-export async function middleware(request) {
+export async function proxy(request) {
     const { pathname } = request.nextUrl;
 
     // --- Protect admin dashboard routes ---
